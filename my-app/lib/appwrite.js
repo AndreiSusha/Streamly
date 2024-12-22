f30ed3ff7931d7eb3ed73078c853fd261347df2d
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-22 17:31:39 +0200

    databaseId added to appwriteConfig

--- my-app/lib/appwrite.js
+++ my-app/lib/appwrite.js
@@ -2,4 +2,5 @@ export const appwriteConfig = {
   endpoint: 'https://cloud.appwrite.io/v1',
   platform: 'com.rn.streamly',
   projectId: '67682ced00358cff1ea2',
+  databaseId: '6768307a0017774e89a3',
 };
