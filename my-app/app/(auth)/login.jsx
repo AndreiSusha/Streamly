8590a02dc0f863728c3b7d02e16321c6baa1654e
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-22 15:02:46 +0200

    update login screen

--- my-app/app/(auth)/login.jsx
+++ my-app/app/(auth)/login.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants';
 
 const LogIn = () => {
   return (
-    <View>
-      <Text>LogIn</Text>
-    </View>
+    <SafeAreaView className="flex-1" style={{ backgroundColor: '#161622' }}>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full justify-start h-full px-4 my-6'>
+          <Image source={images.logo}
+          resizeMode='contain'
+          className='w-[115px] h-[35px]'/>
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Streamly</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
