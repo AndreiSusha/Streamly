df5b4b9facc38f4b032f272914de2a9c33ae14f0
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-22 17:59:03 +0200

    some id's added

--- my-app/lib/appwrite.js
+++ my-app/lib/appwrite.js
@@ -3,4 +3,7 @@ export const appwriteConfig = {
   platform: 'com.rn.streamly',
   projectId: '67682ced00358cff1ea2',
   databaseId: '6768307a0017774e89a3',
+  userCollectionId: '676830ec00068550fd1b',
+  videoCollectionId: '67683166002f5e3a9428',
+  storageId: '676834e20014528004f2',
 };
