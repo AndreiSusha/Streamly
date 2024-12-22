04fb009771492419dc28cb6902b0b949bb92d7b9
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-22 16:46:32 +0200

    register screen created

--- my-app/app/(auth)/register.jsx
+++ my-app/app/(auth)/register.jsx
@@ -1,14 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native';
+import React, { use, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link } from 'expo-router'
+import { images } from '../../constants';
+import InputForm from '../../components/InputForm';
+import Button from '../../components/Button';
 
 const Register = () => {
-  return (
-    <View>
-      <Text>Register
-    
-      </Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
 
-export default Register
+  const [isSubmitting, setIsSubmitting] = useState(false)  
+
+  const submit = () => {
+    console.log('submit', form);
+  };
+
+  return (
+    <SafeAreaView className="flex-1" style={{ backgroundColor: '#161622' }}>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center min-h-[85vh] px-4 ">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Sign up to Streamly
+          </Text>
+          <InputForm
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+          <InputForm
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <InputForm
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <Button 
+          title='Log in'
+          habdlePress={submit}
+          containerStyles='mt-10'
+          isLoading={isSubmitting}
+          />
+
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-gray-100 text-lg font-pregular'>Have an account?</Text>
+            <Link href='/login' className='text-lg text-secondary font-psemibold'>Log in</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Register;
