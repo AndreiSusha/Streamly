837f8d21a94fe58442d10c252089ac334ae8ff08
AndreiSusha <Andrey.Susha@gmail.com>
2024-12-25 20:14:03 +0200

    index file in components folder created

--- my-app/app/(tabs)/home.jsx
+++ my-app/app/(tabs)/home.jsx
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import {images} from '../../constants';
+import {SearchInput, Carousel, EmptyState} from '../../components'
+import { useState } from 'react';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  const [refreshing, setRefreshing] = useState(false);
 
-export default Home
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re call vids -> if any new vids are available
+    setRefreshing(false);
+  };
+
+  return (
+    <SafeAreaView style={{ backgroundColor: '#161622', height: '100%' }}>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <View>
+            <Text className="text-3xl text-white">{item.id}</Text>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className="px-4 my-6 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+              <Text className="text-sm font-pmedium text-gray-100">
+                Welcome back!
+              </Text>
+              <Text className="text-2xl font-psemibold text-white">User</Text>
+              </View>
+
+              <View className='mt-1.5'>
+                <Image
+                  source={images.logo}
+                  className="w-12 h-12"
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+
+          <SearchInput />
+          <View className='w-full flex-1 pt-5 pb-8'>
+          <Text className='text-gray-100 text-lg font-pregular mb-3'>Latest Videos</Text>
+
+          <Carousel posts={[{id: 1}, {id: 2}, {id: 3}] ?? []} />
+          </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+          title="No Videos Found"
+          subtitle="Be the first to upload a video"
+        />
+  )}
+  refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ my-app/components/index.js
@@ -0,0 +1,7 @@
+import Button from './Button';
+import Carousel from './Carousel';
+import InputForm from './InputForm';
+import SearchInput from './SearchInput';
+import EmptyState from './EmptyState';
+
+export { Button, Carousel, InputForm, SearchInput, EmptyState };
