d737288df901fbb0e04e7092e814ce03d69cb3d0
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-08 14:25:14 +0200

    connect to backend

--- my-app/app/(tabs)/home.jsx
+++ my-app/app/(tabs)/home.jsx
@@ -1,32 +1,70 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native';
 
 import { images } from '../../constants';
-import { useAPI } from '../../useAPI';
 import { StatusBar } from 'expo-status-bar';
-import { SearchInput, Carousel, EmptyState } from '../../components';
+import { SearchInput, Carousel, EmptyState, MediaCard } from '../../components';
 
 const Home = () => {
-  const { data: posts, refetch } = useAPI(mediaFiles);
-
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+
+  const mediaFiles = async () => {
+    const response = await fetch('http://192.168.1.241:3000/media', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch media files');
+    }
+
+    return await response.json();
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await mediaFiles();
+        console.log(response);
+        setData(response);
+      } catch (error) {
+        Alert.alert('Error', error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    await refetch();
-    setRefreshing(false);
+    try {
+      const response = await mediaFiles();
+      setData(response);
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setRefreshing(false);
+    }
   };
 
   return (
     <SafeAreaView style={{ backgroundColor: '#161622', height: '100%' }}>
       <FlatList
-        data={posts}
+        data={data}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
-          <View>
-            <Text className="text-3xl text-white">{item.title}</Text>
-          </View>
+          <MediaCard title={item.title} filename={item.filename} user={item.user} />
+          // <View>
+          //   <Text className="text-3xl text-white">{item.title}</Text>
+          // </View>
         )}
         ListHeaderComponent={() => (
           <View className="px-4 my-6 space-y-6">
