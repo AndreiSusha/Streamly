b4b0a1acaedead0ebc9fa895867dc670d9180a66
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-08 14:25:34 +0200

    created file but not sure if i will use it

--- my-app/useAPI.js
+++ my-app/useAPI.js
@@ -1,22 +1,22 @@
 import { Alert } from 'react-native';
 import { useEffect, useState } from 'react';
 
+export const mediaFiles = async () => {
+  const response = await fetch('http://192.168.1.241:3000/media', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (!response.ok) {
+    throw new Error('Failed to fetch media files');
+  }
+  return await response.json();
+};
+
 const useAPI = (fn) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  const mediaFiles = async () => {
-    const response = await fetch('http://192.168.1.241:3000/media', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Failed to fetch media files');
-    }
-    return await response.json();
-  };
 
   // fetch data
   const fetchData = async () => {
