6e518039b8c100f420e468a25fd941f1ae32cab2
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-08 14:24:09 +0200

    new component added

--- my-app/components/index.js
+++ my-app/components/index.js
@@ -3,5 +3,6 @@ import Carousel from './Carousel';
 import InputForm from './InputForm';
 import SearchInput from './SearchInput';
 import EmptyState from './EmptyState';
+import MediaCard from './MediaCard';
 
-export { Button, Carousel, InputForm, SearchInput, EmptyState };
+export { Button, Carousel, InputForm, SearchInput, EmptyState, MediaCard };
