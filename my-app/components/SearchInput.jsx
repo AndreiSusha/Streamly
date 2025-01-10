4f2b6f0d446059071752f004e3912804344ed72e
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-10 19:42:29 +0200

    Updated so that the search works when you press "Search" on the keyboard

--- my-app/components/SearchInput.jsx
+++ my-app/components/SearchInput.jsx
@@ -1,43 +1,46 @@
-import { useState } from "react";
-import { router, usePathname } from "expo-router";
-import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
-import { icons } from "../constants";
+import { useState } from 'react';
+import { router, usePathname } from 'expo-router';
+import { View, TouchableOpacity, Image, TextInput, Alert } from 'react-native';
+import { icons } from '../constants';
 
 const SearchInput = ({ initialQuery }) => {
   const pathname = usePathname();
-  const [query, setQuery] = useState(initialQuery || "");
+  const [query, setQuery] = useState(initialQuery || '');
   const [isFocused, setIsFocused] = useState(false);
 
+  const handleSearch = () => {
+    if (query === '')
+      return Alert.alert(
+        'Missing Query',
+        'Enter something to search for amazing things'
+      );
+
+    if (pathname.startsWith('/search')) router.setParams({ query });
+    else router.push(`/search/${query}`);
+  };
+
   return (
-    <View className={`flex flex-row items-center space-x-4 w-full h-16 px-4 rounded-2xl border-2 ${
-        isFocused ? "border-secondary" : "border-black-200"
-      } bg-black-100`}>
-
-    <TextInput
-      className="text-base mt-0.5 text-white flex-1 font-pregular"
-      value={query}
-      placeholder="Search for something special"
-      placeholderTextColor="#CDCDE0"
-      onChangeText={(e) => setQuery(e)}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-    />
-
-    <TouchableOpacity
-      onPress={() => {
-        if (query === "")
-          return Alert.alert(
-            "Missing Query",
-            "Enter something to search for amazing things"
-          );
-
-        if (pathname.startsWith("/search")) router.setParams({ query });
-        else router.push(`/search/${query}`);
-      }}
+    <View
+      className={`flex flex-row items-center space-x-4 w-full h-16 px-4 rounded-2xl border-2 ${
+        isFocused ? 'border-secondary' : 'border-black-200'
+      } bg-black-100`}
     >
-      <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
-    </TouchableOpacity>
-  </View>
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={query}
+        placeholder="Search for something special"
+        placeholderTextColor="#CDCDE0"
+        onChangeText={(e) => setQuery(e)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        onSubmitEditing={handleSearch}
+        returnKeyType="search"
+      />
+
+      <TouchableOpacity onPress={handleSearch}>
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
