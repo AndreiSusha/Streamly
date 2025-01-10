f19878c2585cde13c8aee22523417b00ea1166b0
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-10 18:14:56 +0200

    start creating serch functionality

--- my-app/app/search/[query].jsx
+++ my-app/app/search/[query].jsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
   return (
     <View>
-      <Text>Search</Text>
+      <Text className="text-2xl font-psemibold text-white mt-1">{query}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
