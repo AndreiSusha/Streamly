29fb147416b9984684e3a9c12074d7e2310041a9
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-10 18:40:36 +0200

    added focus

--- my-app/components/SearchInput.jsx
+++ my-app/components/SearchInput.jsx
@@ -6,14 +6,21 @@ import { icons } from "../constants";
 const SearchInput = ({ initialQuery }) => {
   const pathname = usePathname();
   const [query, setQuery] = useState(initialQuery || "");
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
+    <View className={`flex flex-row items-center space-x-4 w-full h-16 px-4 rounded-2xl border-2 ${
+        isFocused ? "border-secondary" : "border-black-200"
+      } bg-black-100`}>
+
     <TextInput
       className="text-base mt-0.5 text-white flex-1 font-pregular"
       value={query}
       placeholder="Search for something special"
       placeholderTextColor="#CDCDE0"
       onChangeText={(e) => setQuery(e)}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
     />
 
     <TouchableOpacity
