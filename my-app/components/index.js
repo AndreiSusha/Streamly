6205bf907bc28336ae5cb3cea13d025b6a42b8ea
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-11 16:32:17 +0200

    file updated: added infoBox

--- my-app/components/index.js
+++ my-app/components/index.js
@@ -4,5 +4,14 @@ import InputForm from './InputForm';
 import SearchInput from './SearchInput';
 import EmptyState from './EmptyState';
 import MediaCard from './MediaCard';
+import InfoBox from './InfoBox';
 
-export { Button, Carousel, InputForm, SearchInput, EmptyState, MediaCard };
+export {
+  Button,
+  Carousel,
+  InputForm,
+  SearchInput,
+  EmptyState,
+  MediaCard,
+  InfoBox,
+};
