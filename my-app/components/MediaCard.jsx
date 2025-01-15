3611efa8dba1c3969632afd7862586a0655e33a9
AndreiSusha <Andrey.Susha@gmail.com>
2025-01-15 16:39:18 +0200

    avatar update

--- my-app/components/MediaCard.jsx
+++ my-app/components/MediaCard.jsx
@@ -1,18 +1,26 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-
 import { icons } from '../constants';
 
 const MediaCard = ({ title, filename, username, avatar }) => {
+  
+  const getInitials = (name) => (name ? name.charAt(0).toUpperCase() : '?');
+
   return (
     <View className="flex-col items-center px-4 mb-14">
       <View className="flex-row items-start gap-3">
         <View className="justify-center items-center flex-row flex-1">
           <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
-            <Image
-              source={{ uri: avatar || 'https://via.placeholder.com/46' }}
-              className="w-full h-full rounded-lg"
-              resizeMode="cover"
-            />
+            {avatar ? (
+              <Image
+                source={{ uri: avatar }}
+                className="w-full h-full rounded-lg"
+                resizeMode="cover"
+              />
+            ) : (
+              <View className="w-full h-full bg-gray-400 rounded-lg flex justify-center items-center">
+                <Text className="text-white font-bold">{getInitials(username)}</Text>
+              </View>
+            )}
           </View>
           <View className="justify-center flex-1 ml-3 gap-y-1">
             <Text
